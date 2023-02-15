1db071695091269d11c5e38cae51e6b7cdbc86cb
Roni <ronniiii@yahoo.com>
2023-02-15 14:15:58 +0100

    [TEST]Prototype of how data should be stored in firebase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import NotFound from './components/NotFound.js'
-import Home from './pages/Home'
+import React, { useState } from "react";
+import NotFound from "./components/NotFound.js";
+import Home from "./pages/Home";
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-import './App.scss';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.scss";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  function handleUserGenerated(newUser) {
+    setUsers([...users, newUser]);
+  }
+
   return (
     <>
       <Router>
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path='*' element={<NotFound />}/>
+          <Route
+            exact
+            path="/"
+            element={<Home onUserGenerated={handleUserGenerated} />}
+          />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
+      <p>Generated Users:</p>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <p>User ID: {user.id}</p>
+            <p>Username: {user.username}</p>
+            <p>Song Request: {user.song_request}</p>
+            <p>Request Time: {user.request_time}</p>
+          </li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,33 @@
 import React, { useState } from "react";
 import { FaTimes } from "react-icons/fa";
 
-function Home() {
+function Home({ onUserGenerated }) {
   const [isActive, setActive] = useState(false);
+  const [id, setId] = useState(null);
+  const [username, setUsername] = useState("");
 
   const openGuest = () => {
     setActive(!isActive);
+    const newId = generateUniqueId();
+    setId(newId);
   };
   const closeGuest = () => {
     setActive(!isActive);
   };
+
+  function handleUsernameChange(event) {
+    setUsername(event.target.value);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    const newUser = { id, username };
+    onUserGenerated(newUser);
+  }
+
+  function generateUniqueId() {
+    return Math.floor(Math.random() * 1000000);
+  }
+
   return (
     <main>
       <section id="main" className="flex column align-center justify-center">
@@ -19,15 +37,24 @@ function Home() {
           <a href="dj-login">Continue as DJ</a>
         </div>
       </section>
-      <section id="guest" className={isActive ? "open flex align-center justify-center" : "flex align-center justify-center"}>
+      <section
+        id="guest"
+        className={
+          isActive
+            ? "open flex align-center justify-center"
+            : "flex align-center justify-center"
+        }
+      >
         <div className="inner column flex align-center justify-center">
           <FaTimes onClick={closeGuest} />
           <h2>Continue as User</h2>
-          <form action="">
+          <form onSubmit={handleSubmit}>
             <input
               type="text"
               placeholder="Input your nickname"
               id="user_name"
+              value={username}
+              onChange={handleUsernameChange}
             />
             <button type="submit">Continue</button>
           </form>
