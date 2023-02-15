13d7d884f9a6b37f094e03959a6fb648e2613c0c
Roni <ronniiii@yahoo.com>
2023-02-15 13:02:40 +0100

    [REFACTOR]removing unncessary lines of code

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function NotFound() {
   return (
     <main>
+      <section id="err" className="flex column full-width">
         <h1>404</h1>
         <h2>Page Not Found</h2>
+      </section>
     </main>
-  )
+  );
 }
 
-export default NotFound
+export default NotFound;
